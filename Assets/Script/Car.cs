eb94503043195e0272c260b2c69e0273be018101
Zyganadel <121130526+Zyganadel@users.noreply.github.com>
2024-07-26 20:10:11 -0300

    Update car, create test track

--- Assets/Script/Car.cs
+++ Assets/Script/Car.cs
@@ -2,53 +2,59 @@ using Godot;
 
 public partial class Car : VehicleBody3D
 {
-    float maxSteer { get; } = 1;
+	float maxSteer { get; } = 0.5f;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
 
-    public override void _UnhandledInput(InputEvent ie)
-    {
-        base._UnhandledInput(ie);
+	public override void _UnhandledInput(InputEvent ie)
+	{
+		if(ie is InputEventKey)
+		{
+			OnKeyboardEvent();
+		}
 
-        float steer = Input.GetAxis("right", "left");
-        float accel = Input.GetActionStrength("forwards");
-        float brake = Input.GetActionStrength("brake");
+		void OnKeyboardEvent()
+		{
+			float steer = Input.GetAxis("right", "left");
+			float accel = Input.GetActionStrength("forward");
+			float brake = Input.GetActionStrength("brake");
 
-        UpdateSteer();
-        UpdateEnginePower();
-        UpdateBrakes();
+			UpdateSteer();
+			UpdateEnginePower();
+			UpdateBrakes();
 
-        // Anything that happens with steering, do it here.
-        void UpdateSteer()
-        {
-            Steering = steer * maxSteer;
+			// Anything that happens with steering, do it here.
+			void UpdateSteer()
+			{
+				Steering = steer * maxSteer;
 
-            // Also need aircontrol things here too.
-        }
+				// Also need aircontrol things here too.
+			}
 
-        // This will need to have gear management too.
-        void UpdateEnginePower()
-        {
-            // For now, just set the power to a value.
-            float maxpower = 50;
-            EngineForce = accel * maxpower;
-        }
+			// This will need to have gear management too.
+			void UpdateEnginePower()
+			{
+				// For now, just set the power to a value.
+				float maxpower = 100;
+				EngineForce = accel * maxpower;
+			}
 
-        // Handle braking things here.
-        void UpdateBrakes()
-        {
-            float maxbrakes = 35;
-            Brake = brake * maxbrakes;
+			// Handle braking things here.
+			void UpdateBrakes()
+			{
+				float maxbrakes = 15;
+				Brake = brake * maxbrakes;
 
-            // Air control things here.
-        }
-    }
+				// Air control things here.
+			}
+		}
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
 }
