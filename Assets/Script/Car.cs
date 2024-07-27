0f2f00968f05e4eade712e3dfd4c7ea005a9a099
Zyganadel <121130526+Zyganadel@users.noreply.github.com>
2024-07-27 19:11:09 -0300

    Add heat system.

--- Assets/Script/Car.cs
+++ Assets/Script/Car.cs
@@ -5,151 +5,194 @@ using System.Collections.Generic;
 
 public partial class Car : VehicleBody3D
 {
-	[Export] float baseSteering { get; set; } = 0.5f;
-	float steering;
-	[Export] float baseWheelFriction = 0.9f;
-	[Export] float rearWheelFrictionReduc = 0.2f;
-	[Export] float brakeFrictionReduc = 0.3f;
-	[Export] float baseEnginePower { get; set; } = 150; // For gear 1.
-	float enginePower;
-	float finalEnginePower;
-	[Export] float baseBrakePower { get; set; } = 25;
-	float brakePower;
+    // Engine values.
+    [ExportCategory("Engine")]
+    [Export] float baseSteering = 0.5f;
+    [Export] float baseWheelFriction = 0.9f;
+    [Export] float rearWheelFrictionReduc = 0.2f;
+    [Export] float brakeFrictionReduc = 0.3f;
+    [Export] float heatBrakeMod = 0.3f;
+    [Export] float baseEnginePower = 450; // For gear 1.
+    [Export] float heatPowerMod = 1;
+    [Export] float baseBrakePower = 25;
+    [Export] float sdModifier = 0.25f;
+    float steering;
+    float enginePower;
+    float finalEnginePower;
 
-	[Export] Gear[] gears { get; set; } = { Gear.Gear1, Gear.Gear2, Gear.Gear3, Gear.Gear4, Gear.Gear5 };
-	Gear currentGear;
+    [Export] Gear[] gears = { Gear.Gear1, Gear.Gear2, Gear.Gear3, Gear.Gear4, Gear.Gear5 };
+    Gear currentGear;
+    int gearIndex;
+    [Export] VehicleWheel3D[] wheels;
+    VehicleWheel3D rr, lr, rf, lf;
 
-	int gearIndex;
-	[Export] VehicleWheel3D[] wheels;
-	VehicleWheel3D rr, lr, rf, lf;
+    // Heat Mechanics
+    [ExportCategory("Heat")]
+    [Export] float brakeHeat = 0.2f;
+    [Export] float engineHeat = 0.4f;
+    [Export] float heatPerRPMSecond = 75;
+    [Export] float maxHeat = 50; float excessHeat = 0;
+    [Export] float cooling = 0.5f; // Cooling per second.
+    float currentHeat;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		gearIndex = 0;
-		steering = baseSteering;
-		finalEnginePower = baseEnginePower;
-		brakePower = baseBrakePower;
+    float speedLastFrame = 0;
 
-		GetWheels();
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        gearIndex = 0;
+        steering = baseSteering;
+        finalEnginePower = baseEnginePower;
 
-		void GetWheels()
-		{
-			List<VehicleWheel3D> wheelList = new List<VehicleWheel3D>();
-			foreach (Node3D node in GetChildren())
-			{
-				if (node is VehicleWheel3D) { wheelList.Add((VehicleWheel3D)node); }
-			}
-			wheels = wheelList.ToArray();
+        GetWheels();
 
-			lf = (VehicleWheel3D)GetNode("Wheel_LF");
-			rf = (VehicleWheel3D)GetNode("Wheel_RF");
-			rr = (VehicleWheel3D)GetNode("Wheel_RR");
-			lr = (VehicleWheel3D)GetNode("Wheel_LR");
-		}
-	}
+        void GetWheels()
+        {
+            List<VehicleWheel3D> wheelList = new List<VehicleWheel3D>();
+            foreach (Node3D node in GetChildren())
+            {
+                if (node is VehicleWheel3D) { wheelList.Add((VehicleWheel3D)node); }
+            }
+            wheels = wheelList.ToArray();
 
-	public override void _UnhandledInput(InputEvent ie)
-	{
-		if (ie is InputEventKey)
-		{
-			OnKeyboardEvent();
-		}
+            lf = (VehicleWheel3D)GetNode("Wheel_LF");
+            rf = (VehicleWheel3D)GetNode("Wheel_RF");
+            rr = (VehicleWheel3D)GetNode("Wheel_RR");
+            lr = (VehicleWheel3D)GetNode("Wheel_LR");
+        }
+    }
 
-		void OnKeyboardEvent()
-		{
-			float steerInput = Input.GetAxis("right", "left");
-			float accelInput = Input.GetActionStrength("forward");
-			float brakeInput = Input.GetActionStrength("brake");
+    public override void _UnhandledInput(InputEvent ie)
+    {
+        if (ie is InputEventKey)
+        {
+            OnKeyboardEvent();
+        }
 
-			UpdateSteer();
-			UpdateEnginePower();
-			UpdateBrakes();
+        void OnKeyboardEvent()
+        {
+            float steerInput = Input.GetAxis("right", "left");
+            float accelInput = Input.GetActionStrength("forward");
+            float brakeInput = Input.GetActionStrength("brake");
 
-			// Anything that happens with steering, do it here.
-			void UpdateSteer()
-			{
-				Steering = steerInput * steering;
+            UpdateSteer();
+            UpdateEnginePower();
+            UpdateBrakes();
 
-				// Also need aircontrol things here too.
-			}
+            // Anything that happens with steering, do it here.
+            void UpdateSteer()
+            {
+                Steering = steerInput * steering;
 
-			// This will need to have gear management too.
-			void UpdateEnginePower()
-			{
-				EngineForce = accelInput * finalEnginePower;
-			}
+                // Also need aircontrol things here too.
+            }
 
-			// Handle braking things here.
-			void UpdateBrakes()
-			{
-				Brake = brakeInput * brakePower;
+            // This will need to have gear management too.
+            void UpdateEnginePower()
+            {
+                GD.Print($"BP = {baseEnginePower}, EP = {enginePower}, FP = {finalEnginePower}");
 
-				// Make drifts work a bit nicer?
-				float rearFriction = baseWheelFriction - rearWheelFrictionReduc - brakeInput * brakeFrictionReduc;
-				float forwardFriction = baseWheelFriction - brakeInput * brakeFrictionReduc;
-				lf.WheelFrictionSlip = forwardFriction;
-				rf.WheelFrictionSlip = forwardFriction;
-				lr.WheelFrictionSlip = rearFriction;
-				rr.WheelFrictionSlip = rearFriction;
+                finalEnginePower = enginePower * heatPowerMod;
 
-				// Air control things here.
-			}
-		}
-	}
+                EngineForce = accelInput * finalEnginePower;
+            }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		GearCheck();
-		GD.Print(LinearVelocity.Length());
-		GD.Print(GetRPM());
-		GripDebug();
-	}
+            // Handle braking things here.
+            void UpdateBrakes()
+            {
+                float brakePower;
+                float brakeFrictionReduc = this.brakeFrictionReduc;
 
-	void GearCheck()
-	{
-		try { currentGear = gears[gearIndex]; } catch(IndexOutOfRangeException e) { currentGear = gears[0]; GD.PrintErr($"{e.GetType()} at gearcheck."); }
+                // Handle overheating.
+                brakeFrictionReduc += heatBrakeMod * excessHeat;
+                brakePower = baseBrakePower * (1 - heatBrakeMod * excessHeat);
 
-		float speed = LinearVelocity.Length();
-		float RPM = GetRPM();
+                Brake = brakeInput * brakePower;
 
-		bool changed = false;
+                // Make drifts work a bit nicer?
+                float rearFriction = baseWheelFriction - rearWheelFrictionReduc - brakeInput * brakeFrictionReduc;
+                float forwardFriction = baseWheelFriction - brakeInput * brakeFrictionReduc;
+                lf.WheelFrictionSlip = forwardFriction;
+                rf.WheelFrictionSlip = forwardFriction;
+                lr.WheelFrictionSlip = rearFriction;
+                rr.WheelFrictionSlip = rearFriction;
 
-		// Should we change gears?
-		if (RPM > currentGear.upperTransitionSpeed) { gearIndex++; currentGear = gears[gearIndex]; changed = true; }
-		if (RPM < currentGear.lowerTransitionSpeed) { gearIndex--; currentGear = gears[gearIndex]; changed = true; }
+                // Air control things here.
+            }
+        }
+    }
 
-		if (changed)
-		{
-			enginePower = baseEnginePower * currentGear.powerMultiplier;
-		}
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        GearCheck();
+        GD.Print($"RPM is {GetRPM()}, speed is {LinearVelocity.Length()}");
+    }
 
-	float GetRPM()
-	{
-		float avgRpm = 0;
-		foreach (VehicleWheel3D wheel in wheels)
-		{
-			avgRpm += wheel.GetRpm();
-		}
-		avgRpm = avgRpm / 4;
+    void GearCheck()
+    {
+        try { currentGear = gears[gearIndex]; } catch (IndexOutOfRangeException e) { currentGear = gears[0]; GD.PrintErr($"{e.GetType()} at gearcheck."); }
 
-		// Get average rpm relative to gear.
-		return avgRpm / currentGear.powerMultiplier;
-	}
+        float speed = LinearVelocity.Length();
+        float RPM = GetRPM();
 
-	// Speed Drifts!!!
-	void SDCheck(double delta)
-	{
+        // Should we change gears?
+        if (RPM > currentGear.upperTransitionSpeed) { gearIndex++; currentGear = gears[gearIndex]; }
+        if (RPM < currentGear.lowerTransitionSpeed) { gearIndex--; currentGear = gears[gearIndex]; }
 
-	}
+        enginePower = baseEnginePower * currentGear.powerMultiplier;
+    }
 
-	void GripDebug()
-	{
-		foreach (VehicleWheel3D wheel in wheels)
-		{
-			GD.Print(wheel.GetSkidinfo());
-		}
-	}
+    float GetRPM()
+    {
+        float avgRpm = 0;
+        foreach (VehicleWheel3D wheel in wheels)
+        {
+            avgRpm += wheel.GetRpm();
+        }
+        avgRpm = avgRpm / 4;
+
+        // Get average rpm relative to gear.
+        return avgRpm / currentGear.powerMultiplier;
+    }
+
+    float GetAvgGrip()
+    {
+        float totalGrip = 0;
+        foreach (VehicleWheel3D wheel in wheels)
+        {
+            totalGrip += wheel.GetSkidinfo();
+        }
+        return totalGrip / 4;
+    }
+
+    void HeatTick(double delta)
+    {
+        // Actually apply the heat.
+        if (Brake != 0) { TickBrake(); }
+        TickEngine();
+        currentHeat -= (float)(cooling * delta);
+
+        // Now calculate whether or not any adverse effects should happen.
+        float tempExcess = (currentHeat - 50) / 50;
+        excessHeat = Mathf.Clamp(tempExcess, 0, float.MaxValue);
+
+        void TickEngine()
+        {
+            float engineHeat = this.engineHeat;
+            float accelInput = EngineForce / finalEnginePower;
+
+            // If we're accelerating, gain extra heat based on our rpm.
+            if (accelInput != 0)
+            {
+                float extraHeat = (GetRPM() / heatPerRPMSecond) * this.engineHeat;
+                engineHeat += extraHeat;
+            }
+
+            // Apply heat.
+            currentHeat += (float)(engineHeat * delta);
+        }
+
+        void TickBrake() { currentHeat += (float)(delta * brakeHeat); }
+    }
 }
--- Assets/Script/Gear.cs
+++ Assets/Script/Gear.cs
@@ -7,10 +7,10 @@ namespace SDTesting.Assets.Script
     {
         [Export] public float powerMultiplier, upperTransitionSpeed, lowerTransitionSpeed = float.MinValue;
 
-        public static Gear Gear1 = new Gear() { powerMultiplier = 1, upperTransitionSpeed = 100 };
-        public static Gear Gear2 = new Gear() { powerMultiplier = 2f, upperTransitionSpeed = 100, lowerTransitionSpeed = 30 };
-        public static Gear Gear3 = new Gear() { powerMultiplier = 3f, upperTransitionSpeed = 100, lowerTransitionSpeed = 30 };
-        public static Gear Gear4 = new Gear() { powerMultiplier = 4.5f, upperTransitionSpeed = 100, lowerTransitionSpeed = 30 };
-        public static Gear Gear5 = new Gear() { powerMultiplier = 6.5f, upperTransitionSpeed = float.MaxValue, lowerTransitionSpeed = 30 };
+        public static Gear Gear1 = new Gear() { powerMultiplier = 1.5f, upperTransitionSpeed = 80 };
+        public static Gear Gear2 = new Gear() { powerMultiplier = 2f, upperTransitionSpeed = 100, lowerTransitionSpeed = 70 };
+        public static Gear Gear3 = new Gear() { powerMultiplier = 2.5f, upperTransitionSpeed = 125, lowerTransitionSpeed = 90 };
+        public static Gear Gear4 = new Gear() { powerMultiplier = 3.25f, upperTransitionSpeed = 175, lowerTransitionSpeed = 120 };
+        public static Gear Gear5 = new Gear() { powerMultiplier = 4.5f, upperTransitionSpeed = float.MaxValue, lowerTransitionSpeed = 150 };
     }
 }
